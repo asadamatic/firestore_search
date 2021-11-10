5e25eb88186b7cc8c8eb4e5a3cce10dd86cde463
asadamatic <asadamatic@gmail.com>
2021-11-10 21:45:38 +0500

    Fixing Github action for web build.

---
+++ example/lib/example.dart
@@ -0,0 +1,18 @@
+import 'package:example/search_feed.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SearchFeed(),
+    );
+  }
+}
